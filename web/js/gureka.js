7bd9b93b3f185395177804c4488ab0b376ada8f0
Gürel Kaynak <gurelkaynak@gmail.com>
2015-03-08 16:08:24 +0200

    not working

--- web/js/gureka.js
+++ web/js/gureka.js
@@ -1,45 +1,55 @@
 
-/*var Post = React.createClass({*/
-/*getInitialState: function() {*/
-/*return {*/
-/*title: '', */
-/*body: ''    */
-/*};*/
-/*},*/
-
-/*render: function(){*/
-/*return (*/
-/*<div>ho ho </div>*/
-/*);*/
-/*}*/
-/*});*/
 
 /*var Posts = React.createClass({*/
 
 /*});*/
 
-var Blog = React.createClass({
-        /*componentDidMount: function() {*/
-        /*$.get(this.props.source, function(result) {*/
-    /*var lastGist = result[0];*/
-    /*if (this.isMounted()) {*/
-    /*this.setState({*/
-    /*username: lastGist.owner.login,*/
-    /*lastGistUrl: lastGist.html_url*/
-    /*});*/
-    /*}*/
-    /*}.bind(this));*/
-        /*},*/
-    render: function() {
+var Post = React.createClass({
+    render: function(){
         return (
             <div>
-               <h1>Hello Wooooo</h1>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.children}</p>
+            </div>
+        );
+    }
+});
+
+var Blog = React.createClass({
+    getInitialState: function() {
+        return {
+            posts: []
+        }
+    },
+    componentDidMount: function() {
+        $.get(this.props.source, function(result) {
+            console.log(result)
+            this.setState({
+                posts: result
+            });
+            this.render();
+        }.bind(this));
+    },
+    render: function() {
+
+        var posts = this.state.posts.map(function (post){
+            return (
+                <Post title={post.title}>
+                    {post.text}
+                </Post>
+            );
+        });
+
+        return (
+            <div id="posts">
+                {posts}
             </div>
         );
     }
 });
 
 React.render(
-  <Blog />,
+  <Blog source="http://127.0.0.1:8000/.json" />,
   document.getElementById('main')
 );
+
