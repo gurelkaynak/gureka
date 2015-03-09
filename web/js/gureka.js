f87fa1236ca3d17b25aa83a6bfe6074e62bc8807
Gürel Kaynak <gurelkaynak@gmail.com>
2015-03-09 14:48:30 +0200

    pagination

--- web/js/gureka.js
+++ web/js/gureka.js
@@ -1,59 +1,140 @@
 
-/*var Posts = React.createClass({*/
+/*
+ *
+ * General blog parameters
+ *
+ * */
+var blogParams = {
+    'name': "Gürel",
+    'middlename': "Remzi",
+    'lastname': "Kaynak"
+};
 
-/*});*/
+//bootstrap classes
+var bsClasses = {
+    'container': 'container',
+    'row': 'row',
+    'text-muted': 'text-muted',
+    'post-date': 'small',
+    'post': 'col-md-8 col-sm-8 post'
+};
 
+/*
+ *
+ * Single blog post object
+ *
+ */
 var Post = React.createClass({
     render: function(){
 
-        var classString = 'col-md-8 post';
+        var converter = new Showdown.converter();
 
+        rawContent = converter.makeHtml(this.props.children.toString());
         return (
-            <div className={classString}>
+            <div className={bsClasses['post']}>
                 <h1>{this.props.title}</h1>
-                <p>{this.props.children}</p>
+                <p dangerouslySetInnerHTML={{__html: rawContent}}></p>
+                <p className={bsClasses['post-date']}>{this.props.date}</p>
             </div>
         );
     }
 });
 
+var Pagination = React.createClass({
+    render: function(){
+
+        var prev;
+        var next;
+
+        if(this.props.prev){
+            prev = <a id='pagination-prev' onClick={this.props.prevEvent} href='#'>Daha Yeni</a>;
+        }
+        if(this.props.next){
+            next = <a id='pagination-next' onClick={this.props.nextEvent} href='#'>Daha Eski</a>;
+        }
+
+        return (
+            <p>
+                {prev} {next}
+            </p>
+        );
+    }
+});
+
+/*
+ *
+ * Main blog object
+ *
+ * state: 
+ *      posts: blog post array
+ * props:
+ *      source: api url
+ *
+ */
 var Blog = React.createClass({
     getInitialState: function() {
         return {
-            posts: []
+            posts: [],
+            prev: null,
+            next: null
         }
     },
-    componentDidMount: function() {
+    ajaxData: function(url){
         $.ajax({
-            url: this.props.source,
+            url: url,
             dataType: 'json',
             success: function(data) {
-                this.setState({posts: data});
+                if(this.isMounted()){
+                    this.setState({posts: data.results, prev: data.previous, next: data.next});
+                }
             }.bind(this),
             error: function(xhr, status, err) {
-                console.error(this.props.source, status, err.toString());
+                console.error(url, status, err.toString());
             }.bind(this)
         });
     },
+    componentDidMount: function() {
+        this.ajaxData(this.props.source);
+    },
+    handleNext: function(e) {
+        e.preventDefault();
+        this.ajaxData(this.state.next);
+    },
+    handlePrev: function(e) {
+        e.preventDefault();
+        this.ajaxData(this.state.prev);
+    },
     render: function() {
         var posts = this.state.posts.map(function (post){
+            date = new Date(post.created_date);
+            dmyStr = date.toLocaleDateString();
+            timeStr = date.toLocaleTimeString();
+
+            str = dmyStr + " " + timeStr;
             return (
-                <Post title={post.title}>
+                <Post date={str} title={post.title}>
                     {post.text}
                 </Post>
             );
         });
 
         return (
-            <div id="posts">
-                {posts}
+            <div className={bsClasses['container']}>
+                <h1>{[blogParams['name'] + " ", <span className={bsClasses['text-muted']}>{blogParams['middlename']}</span>, " " + blogParams['lastname'] + " Blog"]}</h1>
+                <hr />
+                <div className={bsClasses['row']}>
+                    <div id="posts">
+                        {posts}
+                    </div>
+                </div>
+                <Pagination prevEvent={this.handlePrev.bind(this)} nextEvent={this.handleNext.bind(this)} prev={this.state.prev} next={this.state.next} />
             </div>
         );
     }
 });
 
 React.render(
-  <Blog source="http://127.0.0.1:8000/?format=json" />,
+  <Blog source="http://localhost:8000/api/post_list/?format=json" />,
   document.getElementById('main')
 );
 
